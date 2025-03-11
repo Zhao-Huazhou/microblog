8ecb98fdcae5fa9ea3f3c2c43f0d8985bd62fd67
ZhaoHuazhou <aiden_zhao@163.com>
2025-03-11 15:05:07 +0800

    User Loader Function

--- app/models.py
+++ app/models.py
@@ -4,8 +4,10 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -34,3 +36,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"<Post {self.body}>"
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
