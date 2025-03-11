2b0b9e0493d88e7c9e04e3bbef4242eaa0bb2d46
ZhaoHuazhou <aiden_zhao@163.com>
2025-03-11 17:43:32 +0800

    complete login

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,19 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, login_required
 import sqlalchemy as sa
 from app import db
 from app.models import User
 from flask_login import logout_user
+from flask import request
+from urllib.parse import urlsplit
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Miguel'}
+    # user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -21,7 +24,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -36,7 +39,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title="Sign In", form=form)
 
 @app.route('/logout')
