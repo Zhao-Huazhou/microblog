3b3e43a7f9953b858244ff0b95d37a050c6e5c6f
ZhaoHuazhou <aiden_zhao@163.com>
2025-03-12 11:05:40 +0800

    complete user last_seen

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,7 @@ from app.models import User
 from flask_login import logout_user
 from flask import request
 from urllib.parse import urlsplit
+from datetime import datetime, timezone
 
 @app.route('/')
 @app.route('/index')
@@ -73,3 +74,9 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
