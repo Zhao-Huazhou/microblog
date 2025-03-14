b2f6acd9a236d295708716b3199f1bcfe24c8acf
ZhaoHuazhou <aiden_zhao@163.com>
2025-03-14 11:43:00 +0800

    Pagination of Blog Posts

--- config.py
+++ config.py
@@ -5,9 +5,12 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['qq934131691@gmail.com']
+
+    POSTS_PER_PAGE = 3
